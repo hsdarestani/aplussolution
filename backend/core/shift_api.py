7ace8988d2163de2d38a83f45c17554783b307b7
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-07-31 12:42:39 +0330

    Link staffing demand to customer order

--- backend/core/shift_api.py
+++ backend/core/shift_api.py
@@ -5,8 +5,9 @@ class ShiftApiSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.name', read_only=True)
     location_name = serializers.CharField(source='location.name', read_only=True)
     position_name = serializers.CharField(source='position.name', read_only=True)
+    order_title = serializers.CharField(source='order.title', read_only=True)
     open_count = serializers.IntegerField(read_only=True)
     filled_count = serializers.IntegerField(read_only=True)
     class Meta:
         model = Shift
-        fields = ['id','client','client_name','location','location_name','position','position_name','starts_at','ends_at','break_minutes','status','notes','required_count','open_count','filled_count']
+        fields = ['id','order','order_title','client','client_name','location','location_name','position','position_name','starts_at','ends_at','break_minutes','status','notes','required_count','open_count','filled_count']
