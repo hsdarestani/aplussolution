11faa4a629d22d3ecde9a444a6fd79336b7fc6b9
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-08-16 10:12:27 +0330

    Expose effective shift timezone

--- backend/core/shift_api.py
+++ backend/core/shift_api.py
@@ -8,6 +8,7 @@ from .shift_slots import ShiftSlot
 class ShiftApiSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.name', read_only=True)
     location_name = serializers.CharField(source='location.name', read_only=True)
+    location_timezone = serializers.CharField(source='location.timezone', read_only=True)
     position_name = serializers.CharField(source='position.name', read_only=True)
     order_title = serializers.CharField(source='order.title', read_only=True)
     open_count = serializers.IntegerField(read_only=True)
@@ -22,7 +23,7 @@ class ShiftApiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shift
         fields = [
-            'id', 'order', 'order_title', 'client', 'client_name', 'location', 'location_name',
+            'id', 'order', 'order_title', 'client', 'client_name', 'location', 'location_name', 'location_timezone',
             'position', 'position_name', 'position_color', 'shift_color', 'location_color',
             'starts_at', 'ends_at', 'break_minutes', 'status', 'notes',
             'required_count', 'open_count', 'filled_count', 'assignments', 'required_tags', 'my_confirmation',
